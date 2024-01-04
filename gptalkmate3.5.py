03c25910bec3ed7387075cac489a9e0f909da486
alteralter33 <154977065+alteralter33@users.noreply.github.com>
2024-01-04 17:54:49 +0900

    Update gptalkmate3.5.py

--- gptalkmate3.5.py
+++ gptalkmate3.5.py
@@ -14,7 +14,7 @@ for msg in st.session_state.messages:
 if prompt := st.chat_input():
 
 
-    client = OpenAI(api_key="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")
+    client = OpenAI(api_key="")
     st.session_state.messages.append({"role": "user", "content": prompt})
     st.chat_message("user").write(prompt)
     response = client.chat.completions.create(model="gpt-3.5-turbo-1106", messages=st.session_state.messages)
