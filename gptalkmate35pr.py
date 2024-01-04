f56a2a3c3f7efc4361228a8e38a70057f1da6a85
alteralter33 <154977065+alteralter33@users.noreply.github.com>
2024-01-04 18:19:35 +0900

    Update gptalkmate35pr.py

--- gptalkmate35pr.py
+++ gptalkmate35pr.py
@@ -7,7 +7,7 @@ with st.sidebar:
     "[View the source code](https://github.com/streamlit/llm-examples/blob/main/Chatbot.py)"
     "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
     
-st.title("💬 Chatbot GPT3.5-PR")
+st.title("💬 Chatbot GPTalkmate3.5-PR")
 st.caption("🚀 A streamlit chatbot powered by OpenAI LLM")
 if "messages" not in st.session_state:
     st.session_state["systemprompt"] = [{"role": "system", "content": "Now, you're a Japanese conversation practice assistant. Please assess the user's language proficiency based on their vocabulary, grammar complexity, sentence length, and grammar errors. Respond at a level equivalent to the user's language proficiency. For instance, if the user's vocabulary and grammar are simple, adjust your response to match that simplicity; if they're more complex, respond accordingly. Additionally, in your replies, guide the user for the next part of the conversation by asking questions. Also, try to simulate everyday conversations—avoid overly lengthy sentences or excessive consecutive questioning."}]
