0cc9e5af50e79504bd9a791ae41ccb27b4fe09ae
alteralter33 <154977065+alteralter33@users.noreply.github.com>
2024-01-04 18:43:08 +0900

    Update gptalkmate35ft.py

--- gptalkmate35ft.py
+++ gptalkmate35ft.py
@@ -22,10 +22,10 @@ if prompt := st.chat_input():
         st.info("Please add your OpenAI API key to continue.")
         st.stop()
 
-    client = OpenAI()
+    client = OpenAI(openai_api_key)
     st.session_state.messages.append({"role": "user", "content": prompt})
     st.chat_message("user").write(prompt)
-    response = client.chat.completions.create(model=openai_api_key, messages=st.session_state.messages)
+    response = client.chat.completions.create(model="ft:gpt-3.5-turbo-1106:hdi-lab::8bVPgYxe", messages=st.session_state.messages)
     msg = response.choices[0].message.content
     st.session_state.messages.append({"role": "assistant", "content": msg})
     st.chat_message("assistant").write(msg)
